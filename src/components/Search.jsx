ceaa3926b9ae6f42945c3cc098dc9a64ead63b40
Sascha Mckayla <mckaylasascha@gmail.com>
2026-05-16 14:45:15 +0300

    feat:add placeholder

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,9 +4,11 @@ function Search({ search, setSearch }) {
   return (
     <div className="searchbar">
       <label htmlFor="search">Search Plants:</label>
+      
       <input
         type="text"
         id="search"
+        placeholder="Type a name to search..."
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
