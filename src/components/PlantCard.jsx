02110bf1bbf6c69b7288f1c32f843c79e4af084e
Sascha Mckayla <mckaylasascha@gmail.com>
2026-05-16 12:56:18 +0300

    feat:Display plant details and stock toggle

--- src/components/PlantCard.jsx
+++ src/components/PlantCard.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
-function PlantCard() {
+function PlantCard({plant}) {
+   const [inStock, setInStock] = useState(true);
+
   return (
     <li className="card" data-testid="plant-item">
       <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
+      
       {true ? (
-        <button className="primary">In Stock</button>
+        <button onClick={() => setInStock(false)} className="primary">In Stock</button>
       ) : (
         <button>Out of Stock</button>
       )}
