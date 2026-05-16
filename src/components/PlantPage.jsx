7f788da082ba80c0b4b9bbfd4191add9db59946d
Sascha Mckayla <mckaylasascha@gmail.com>
2026-05-16 12:23:58 +0300

    feat: Add plants and search state

--- src/components/PlantPage.jsx
+++ src/components/PlantPage.jsx
@@ -4,6 +4,9 @@ import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+const [search, setSearch] = useState("");
+
   useEffect(() => {
   fetch("http://localhost:6001/plants")
     .then((res) => res.json())
